4960655b9fca272c3ea4b4bbff9c81da839826f5
Haik Simon <land@lx-pool.gsi.de>
2021-04-24 19:12:30 +0200

    clang-format -i on macros

--- r3b/unpack/s515/beam/calib_sci2.C
+++ r3b/unpack/s515/beam/calib_sci2.C
@@ -12,90 +12,86 @@
 
 struct EXT_STR_h101_t
 {
-	EXT_STR_h101_unpack_t unpack;
-	EXT_STR_h101_SCI2_t s2;
+    EXT_STR_h101_unpack_t unpack;
+    EXT_STR_h101_SCI2_t s2;
 };
 
 void calib_sci2(Int_t RunId = 1)
 {
-  TString runNumber=Form ("%03d", RunId);
-	TStopwatch timer;
-	timer.Start();
+    TString runNumber = Form("%03d", RunId);
+    TStopwatch timer;
+    timer.Start();
 
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
-  std::ostringstream oss;
-  oss << std::put_time(&tm, "%Y%m%d_%H%M%S");
-	
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y%m%d_%H%M%S");
 
-	const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
-	
-	// --- ------------------------------------------------------ ---	
-	// --- check and modify if necessary ------------------------ ---
-	// --- for unpacking ---------------------------------------- ---
-	// --- ------------------------------------------------------ ---	
-	// --- for local computer
-	TString filename = "~/data/s515/main*.lmd --allow-errors --input-buffer=50Mi"; 
-  TString ucesb_dir  = getenv("UCESB_DIR"); 
-  TString ucesb_path = ucesb_dir + "/../upexps/202104_s515/202104_s515";  
-  ucesb_path.ReplaceAll("//", "/");
-	
-	// --- land account
-	//TString filename = "/d/land5/202104_s515/lmd/main*.lmd --allow-errors --input-buffer=50Mi"; 
-	//TString filename = " --stream=lxir136:9001 --allow-errors --input-buffer=50Mi"; 
-	//TString ucesb_path = "/u/land/fake_cvmfs/9.13/upexps/202104_s515/202104_s515";
-	
-	TString ntuple_options = "RAW:SCITWO";                    // ntuple options for stitched data  
-	//TString ntuple_options = "RAW,time_stitch=1000"; // ntuple_options for raw data
+    const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
 
- 
-	// --- ------------------------------------------------------ ---	
-	// --- check and modify if necessary ------------------------ ---
-  // --- for online run --------------------------------------- --- 
-	// --- ------------------------------------------------------ ---	
-	TString output_path = "~/data/s515/";
-	//TString output_path = "/d/land5/202104_s515/rootfiles/beam/";
-	TString outputFilename = output_path+"s515_tcal_sci2_" + oss.str() + ".root";
-  const Int_t refresh = 1;                 
-  Int_t port = 5555;
-	
+    // --- ------------------------------------------------------ ---
+    // --- check and modify if necessary ------------------------ ---
+    // --- for unpacking ---------------------------------------- ---
+    // --- ------------------------------------------------------ ---
+    // --- for local computer
+    TString filename = "~/data/s515/main*.lmd --allow-errors --input-buffer=50Mi";
+    TString ucesb_dir = getenv("UCESB_DIR");
+    TString ucesb_path = ucesb_dir + "/../upexps/202104_s515/202104_s515";
+    ucesb_path.ReplaceAll("//", "/");
 
-  EXT_STR_h101_t ucesb_struct;
-	R3BUcesbSource* source = new R3BUcesbSource(filename, ntuple_options,	ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
-	source->SetMaxEvents(nev);
-	source->AddReader(new R3BUnpackReader(&ucesb_struct.unpack,offsetof(EXT_STR_h101_t, unpack)));
-  source->AddReader( new R3BSci2Reader (&ucesb_struct.s2, offsetof(EXT_STR_h101_t, s2)) );
+    // --- land account
+    // TString filename = "/d/land5/202104_s515/lmd/main*.lmd --allow-errors --input-buffer=50Mi";
+    // TString filename = " --stream=lxir136:9001 --allow-errors --input-buffer=50Mi";
+    // TString ucesb_path = "/u/land/fake_cvmfs/9.13/upexps/202104_s515/202104_s515";
 
-  /* Create online run ------------------------------------ */
+    TString ntuple_options = "RAW:SCITWO"; // ntuple options for stitched data
+    // TString ntuple_options = "RAW,time_stitch=1000"; // ntuple_options for raw data
+
+    // --- ------------------------------------------------------ ---
+    // --- check and modify if necessary ------------------------ ---
+    // --- for online run --------------------------------------- ---
+    // --- ------------------------------------------------------ ---
+    TString output_path = "~/data/s515/";
+    // TString output_path = "/d/land5/202104_s515/rootfiles/beam/";
+    TString outputFilename = output_path + "s515_tcal_sci2_" + oss.str() + ".root";
+    const Int_t refresh = 1;
+    Int_t port = 5555;
+
+    EXT_STR_h101_t ucesb_struct;
+    R3BUcesbSource* source =
+        new R3BUcesbSource(filename, ntuple_options, ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
+    source->SetMaxEvents(nev);
+    source->AddReader(new R3BUnpackReader(&ucesb_struct.unpack, offsetof(EXT_STR_h101_t, unpack)));
+    source->AddReader(new R3BSci2Reader(&ucesb_struct.s2, offsetof(EXT_STR_h101_t, s2)));
+
+    /* Create online run ------------------------------------ */
 #define RUN_ONLINE
 #define USE_HTTP_SERVER
 #ifdef RUN_ONLINE
-  FairRunOnline* run = new FairRunOnline(source);
-  run->SetRunId(RunId);
+    FairRunOnline* run = new FairRunOnline(source);
+    run->SetRunId(RunId);
 #ifdef USE_HTTP_SERVER
-  run->ActivateHttpServer(refresh);
+    run->ActivateHttpServer(refresh);
 #endif
 #else
-  /* Create analysis run ---------------------------------- */
-  FairRunAna* run = new FairRunAna();
+    /* Create analysis run ---------------------------------- */
+    FairRunAna* run = new FairRunAna();
 #endif
 
     run->SetOutputFile(outputFilename.Data());
 
-
-	/* Runtime data base ------------------------------------ */
-	FairRuntimeDb* rtdb = run->GetRuntimeDb();
-	Bool_t kParameterMerged = kTRUE;
-	TString parFileName    = "parameter/tcal_s2.root";  // name of parameter file	"+runNumber+"
-	FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
-	parOut->open(parFileName);
+    /* Runtime data base ------------------------------------ */
+    FairRuntimeDb* rtdb = run->GetRuntimeDb();
+    Bool_t kParameterMerged = kTRUE;
+    TString parFileName = "parameter/tcal_s2.root"; // name of parameter file	"+runNumber+"
+    FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+    parOut->open(parFileName);
     rtdb->setFirstInput(parOut);
     rtdb->setOutput(parOut);
 
-
-	/* Calibrate Sci2 ---------------------------------------- */
+    /* Calibrate Sci2 ---------------------------------------- */
     const Int_t updateRate = 1000;
-    const Int_t minStats = 10000;        // minimum number of entries for TCAL calibration
+    const Int_t minStats = 10000; // minimum number of entries for TCAL calibration
     const Int_t trigger = 1;
     R3BSci2Mapped2CalPar* s2Calibrator = new R3BSci2Mapped2CalPar("R3BSci2Mapped2CalPar", 1);
     s2Calibrator->SetUpdateRate(updateRate);
@@ -103,31 +99,29 @@ void calib_sci2(Int_t RunId = 1)
     s2Calibrator->SetTrigger(trigger);
     s2Calibrator->SetNofModules(1, 3); // dets, bars(incl. master trigger)
     run->AddTask(s2Calibrator);
-	/* Calibrate Los - END */
+    /* Calibrate Los - END */
 
+    /* Initialize ------------------------------------------- */
+    run->Init();
+    FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+    FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
+    FairLogger::GetLogger()->SetLogScreenLevel("ERROR");
+    /* ------------------------------------------------------ */
 
-	/* Initialize ------------------------------------------- */
-	run->Init();
-	FairLogger::GetLogger()->SetLogScreenLevel("INFO");
-	FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
-	FairLogger::GetLogger()->SetLogScreenLevel("ERROR");
-	/* ------------------------------------------------------ */
+    /* Run -------------------------------------------------- */
+    run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
+    if (rtdb->getCurrentRun())
+        cout << "have run" << endl;
+    else
+        cout << "have no run" << endl;
+    rtdb->saveOutput();
+    /* ------------------------------------------------------ */
 
-
-	/* Run -------------------------------------------------- */
-	run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
-	if (rtdb->getCurrentRun()) cout << "have run" << endl;
-	else cout << "have no run" << endl;
-	rtdb->saveOutput();
-	/* ------------------------------------------------------ */
-
-	timer.Stop();
-	Double_t rtime = timer.RealTime();
-	Double_t ctime = timer.CpuTime();
-	cout << endl << endl;
-	cout << "Macro finished succesfully." << endl;
-	cout << "Output file is " << outputFilename << endl;
-	cout << "Real time " << rtime << " s, CPU time " << ctime << " s"
-		     << endl << endl;
+    timer.Stop();
+    Double_t rtime = timer.RealTime();
+    Double_t ctime = timer.CpuTime();
+    cout << endl << endl;
+    cout << "Macro finished succesfully." << endl;
+    cout << "Output file is " << outputFilename << endl;
+    cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl << endl;
 }
-
--- r3b/unpack/s515/beam/online_beamId.C
+++ r3b/unpack/s515/beam/online_beamId.C
@@ -1,208 +1,205 @@
-#include <sstream>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 struct EXT_STR_h101_t
 {
-	EXT_STR_h101_unpack_t unpack;
-	EXT_STR_h101_TPAT_t TPAT;
-	EXT_STR_h101_LOS_t los;	
-	EXT_STR_h101_timestamp_master_t timestamp_master;
-	EXT_STR_h101_SCI2_t s2;
+    EXT_STR_h101_unpack_t unpack;
+    EXT_STR_h101_TPAT_t TPAT;
+    EXT_STR_h101_LOS_t los;
+    EXT_STR_h101_timestamp_master_t timestamp_master;
+    EXT_STR_h101_SCI2_t s2;
 };
 
 void online_beamId()
 {
-	TStopwatch timer;
-  timer.Start();
-    
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
-  std::ostringstream oss;
-  oss << std::put_time(&tm, "%Y%m%d_%H%M%S");
+    TStopwatch timer;
+    timer.Start();
 
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y%m%d_%H%M%S");
 
-	// --- ------------------------------------------------------ ---	
-	// --- check and modify if necessary ------------------------ ---
-	// --- for unpacking ---------------------------------------- ---
-	// --- ------------------------------------------------------ ---	
-	// --- for local computer
-	//TString filename = "~/data/s515/main*.lmd --allow-errors --input-buffer=50Mi"; 
-  //TString ucesb_dir  = getenv("UCESB_DIR"); 
-  //TString ucesb_path = ucesb_dir + "/../upexps/202104_s515/202104_s515";  
-  //ucesb_path.ReplaceAll("//", "/");
-	
-	// --- land account
-	//TString filename = "/d/land5/202104_s515/lmd/main*.lmd --allow-errors --input-buffer=50Mi"; 
-	TString filename = " --stream=lxlanddaq01:9001 --allow-errors --input-buffer=50Mi"; //PSPx and Incoming ID online analysis 
-	TString ucesb_path = "/u/land/fake_cvmfs/9.13/upexps/202104_s515/202104_s515";
-	
-	TString ntuple_options = "RAW";                    // ntuple options for stitched data  
-	//TString ntuple_options = "RAW,time_stitch=1000"; // ntuple_options for raw data
+    // --- ------------------------------------------------------ ---
+    // --- check and modify if necessary ------------------------ ---
+    // --- for unpacking ---------------------------------------- ---
+    // --- ------------------------------------------------------ ---
+    // --- for local computer
+    // TString filename = "~/data/s515/main*.lmd --allow-errors --input-buffer=50Mi";
+    // TString ucesb_dir  = getenv("UCESB_DIR");
+    // TString ucesb_path = ucesb_dir + "/../upexps/202104_s515/202104_s515";
+    // ucesb_path.ReplaceAll("//", "/");
 
- 
-	// --- ------------------------------------------------------ ---	
-	// --- check and modify if necessary ------------------------ ---
-  // --- for online run --------------------------------------- --- 
-	// --- ------------------------------------------------------ ---	
-	TString output_path = "~/data/s515/";
-	//TString output_path = "/d/land5/202104_s515/rootfiles/beam/";
-	TString outputFilename = output_path+"s515_beamid_" + oss.str() + ".root";
-  const Int_t refresh = 1;                 
-  Int_t port = 5555;
+    // --- land account
+    // TString filename = "/d/land5/202104_s515/lmd/main*.lmd --allow-errors --input-buffer=50Mi";
+    TString filename =
+        " --stream=lxlanddaq01:9001 --allow-errors --input-buffer=50Mi"; // PSPx and Incoming ID online analysis
+    TString ucesb_path = "/u/land/fake_cvmfs/9.13/upexps/202104_s515/202104_s515";
 
-  
-	// Turn off automatic backtrace 
-  gSystem->ResetSignals();   
+    TString ntuple_options = "RAW"; // ntuple options for stitched data
+    // TString ntuple_options = "RAW,time_stitch=1000"; // ntuple_options for raw data
 
-    
-	// --- ------------------------------------------------------ ---	
-	// --- number of events to read, ifnev = -1 read until CTRL+C --- 
-	// --- ------------------------------------------------------ ---	
-  const Int_t nev = -1; 
-	const Int_t RunId = 1;   
- 
+    // --- ------------------------------------------------------ ---
+    // --- check and modify if necessary ------------------------ ---
+    // --- for online run --------------------------------------- ---
+    // --- ------------------------------------------------------ ---
+    TString output_path = "~/data/s515/";
+    // TString output_path = "/d/land5/202104_s515/rootfiles/beam/";
+    TString outputFilename = output_path + "s515_beamid_" + oss.str() + ".root";
+    const Int_t refresh = 1;
+    Int_t port = 5555;
 
-	// --- -------------------------------------------------------- ---	
-  // --- Create source using ucesb for input -------------------- ---
-	// --- -------------------------------------------------------- ---	
-  EXT_STR_h101_t ucesb_struct;
-	R3BUcesbSource* source = new R3BUcesbSource(filename, ntuple_options,ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
-	source->SetMaxEvents(nev);
+    // Turn off automatic backtrace
+    gSystem->ResetSignals();
 
-	// --- -------------------------------------------------------- ---	
-	// --- Add Readers -------------------------------------------- ---
-	// --- -------------------------------------------------------- ---	
-	source->AddReader(new R3BUnpackReader(&ucesb_struct.unpack,offsetof(EXT_STR_h101_t, unpack)));
-	source->AddReader(new R3BTrloiiTpatReader((EXT_STR_h101_TPAT_t *)&ucesb_struct.TPAT,offsetof(EXT_STR_h101, TPAT)));	
-  source->AddReader(new R3BTimestampMasterReader((EXT_STR_h101_timestamp_master_t *)&ucesb_struct.timestamp_master,offsetof(EXT_STR_h101, timestamp_master)));
-  source->AddReader( new R3BLosReader (&ucesb_struct.los, offsetof(EXT_STR_h101_t, los)) );
-  source->AddReader( new R3BSci2Reader (&ucesb_struct.s2, offsetof(EXT_STR_h101_t, s2)) );
+    // --- ------------------------------------------------------ ---
+    // --- number of events to read, ifnev = -1 read until CTRL+C ---
+    // --- ------------------------------------------------------ ---
+    const Int_t nev = -1;
+    const Int_t RunId = 1;
 
-	// --- -------------------------------------------------------- ---	
-	// --- Create online run -------------------------------------- ---
-	// --- -------------------------------------------------------- ---	
-	FairRunOnline* run = new FairRunOnline(source);
-  run->SetRunId(RunId);
-  run->SetSink(new FairRootFileSink(outputFilename));
-  //run->SetOutputFile(outputFileName.Data());
-  run->ActivateHttpServer(refresh,port);
+    // --- -------------------------------------------------------- ---
+    // --- Create source using ucesb for input -------------------- ---
+    // --- -------------------------------------------------------- ---
+    EXT_STR_h101_t ucesb_struct;
+    R3BUcesbSource* source =
+        new R3BUcesbSource(filename, ntuple_options, ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
+    source->SetMaxEvents(nev);
 
-	// --- -------------------------------------------------------- ---	
-	// --- Parameter IO setup ------------------------------------- ---
-	// --- -------------------------------------------------------- ---	
-  auto rtdb = run->GetRuntimeDb();
+    // --- -------------------------------------------------------- ---
+    // --- Add Readers -------------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    source->AddReader(new R3BUnpackReader(&ucesb_struct.unpack, offsetof(EXT_STR_h101_t, unpack)));
+    source->AddReader(new R3BTrloiiTpatReader((EXT_STR_h101_TPAT_t*)&ucesb_struct.TPAT, offsetof(EXT_STR_h101, TPAT)));
+    source->AddReader(new R3BTimestampMasterReader((EXT_STR_h101_timestamp_master_t*)&ucesb_struct.timestamp_master,
+                                                   offsetof(EXT_STR_h101, timestamp_master)));
+    source->AddReader(new R3BLosReader(&ucesb_struct.los, offsetof(EXT_STR_h101_t, los)));
+    source->AddReader(new R3BSci2Reader(&ucesb_struct.s2, offsetof(EXT_STR_h101_t, s2)));
 
-	// LOS CALIBRATION PARAMETERS
-	auto parIOlos = new FairParRootFileIo(false); 
-  parIOlos->open("parameter/tcal_los_pulser.root");
-	rtdb->setFirstInput(parIOlos);
+    // --- -------------------------------------------------------- ---
+    // --- Create online run -------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    FairRunOnline* run = new FairRunOnline(source);
+    run->SetRunId(RunId);
+    run->SetSink(new FairRootFileSink(outputFilename));
+    // run->SetOutputFile(outputFileName.Data());
+    run->ActivateHttpServer(refresh, port);
 
-	// S2 CALIBRATION PARAMETER if ascii
-	// auto parIOs2 = new FairParAsciiFileIo();
-	// parIOs2->open("parameter/tcal_s2.par","in");
-	// rtdb->setSecondInput(parIOs2);
+    // --- -------------------------------------------------------- ---
+    // --- Parameter IO setup ------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    auto rtdb = run->GetRuntimeDb();
 
-	// S2 CALIBRATION PARAMETER 
-	auto parIOs2 = new FairParRootFileIo(false);
-	//parIOs2->open("parameter/tcal_s2.root");
-	//rtdb->setSecondInput(parIOs2);
-	
-	rtdb->print();
-  rtdb->addRun(RunId);
-  rtdb->getContainer("LosTCalPar");
-  rtdb->setInputVersion(RunId, (char*)"LosTCalPar", 1, 1);
-  //rtdb->getContainer("Sci2TCalPar");
-  //rtdb->setInputVersion(RunId, (char*)"Sci2TCalPar", 1, 1);
+    // LOS CALIBRATION PARAMETERS
+    auto parIOlos = new FairParRootFileIo(false);
+    parIOlos->open("parameter/tcal_los_pulser.root");
+    rtdb->setFirstInput(parIOlos);
 
+    // S2 CALIBRATION PARAMETER if ascii
+    // auto parIOs2 = new FairParAsciiFileIo();
+    // parIOs2->open("parameter/tcal_s2.par","in");
+    // rtdb->setSecondInput(parIOs2);
 
-	// --- -------------------------------------------------------- ---	
-  // --- Add analysis tasks ------------------------------------- --- 
-	// --- -------------------------------------------------------- ---	
+    // S2 CALIBRATION PARAMETER
+    auto parIOs2 = new FairParRootFileIo(false);
+    // parIOs2->open("parameter/tcal_s2.root");
+    // rtdb->setSecondInput(parIOs2);
 
-  // LOS Mapped => Cal
-  R3BLosMapped2Cal* losMapped2Cal = new R3BLosMapped2Cal("LosTCalPar", 1);
-  losMapped2Cal->SetNofModules(1,8);
-  losMapped2Cal->SetTrigger(1);
-  run->AddTask(losMapped2Cal);
-  
-	// S2 Mapped -> Tcal
-	R3BSci2Mapped2Tcal* s2Mapped2Tcal = new R3BSci2Mapped2Tcal("Sci2Map2Tcal",1);
-	// run->AddTask(s2Mapped2Tcal); 
- 
-	// --- -------------------------------------------------------- ---	
-	// --- Add online tasks --------------------------------------- --- 
-	// --- -------------------------------------------------------- ---	
+    rtdb->print();
+    rtdb->addRun(RunId);
+    rtdb->getContainer("LosTCalPar");
+    rtdb->setInputVersion(RunId, (char*)"LosTCalPar", 1, 1);
+    // rtdb->getContainer("Sci2TCalPar");
+    // rtdb->setInputVersion(RunId, (char*)"Sci2TCalPar", 1, 1);
 
-	// --- LOS STANDALONE
-  R3BOnlineSpectraLosStandalone* r3bOnlineSpectra=new R3BOnlineSpectraLosStandalone("OnlineSpectraLosStandalone", 1); 
-	r3bOnlineSpectra->SetNofLosModules(1); // 1 or 2 LOS detectors  
-  //  Set parameters for X,Y calibration
-  //  (offsetX, offsetY,VeffX,VeffY)
-  r3bOnlineSpectra->SetLosXYTAMEX(0,0,1,1,0,0,1,1);
-  r3bOnlineSpectra->SetLosXYMCFD(1.011,1.216,1.27,1.88,0,0,1,1);//(0.9781,1.152,1.5,1.5,0,0,1,1);                       
-  r3bOnlineSpectra->SetLosXYToT(-0.002373,0.007423,2.27,3.22,0,0,1,1);//(-0.02054,-0.02495,2.5,3.6,0,0,1,1);
-  r3bOnlineSpectra->SetEpileup(350.);  // Events with ToT>Epileup are not considered
-  r3bOnlineSpectra->SetTrigger(1);     // -1 = no trigger selection
-	r3bOnlineSpectra->SetTpat(0);        // if 0, no tpat selection
-  run->AddTask( r3bOnlineSpectra );
+    // --- -------------------------------------------------------- ---
+    // --- Add analysis tasks ------------------------------------- ---
+    // --- -------------------------------------------------------- ---
 
-	// --- SCI2 STANDALONE
-	R3BOnlineSpectraSci2* s2online = new R3BOnlineSpectraSci2();
-	s2online->SetNbDetectors(1);
-	s2online->SetNbChannels(3);
-	run->AddTask(s2online);		
+    // LOS Mapped => Cal
+    R3BLosMapped2Cal* losMapped2Cal = new R3BLosMapped2Cal("LosTCalPar", 1);
+    losMapped2Cal->SetNofModules(1, 8);
+    losMapped2Cal->SetTrigger(1);
+    run->AddTask(losMapped2Cal);
 
-	// --- LOS VS SCI2
-  R3BOnlineSpectraLosVsSci2* loss2online =new R3BOnlineSpectraLosVsSci2("OnlineSpectraLosVsSci2", 1); 
-	loss2online->SetNofLosModules(1); // 1 or 2 LOS detectors  
-  //  Set parameters for X,Y calibration
-  //  (offsetX, offsetY,VeffX,VeffY)
-  loss2online->SetLosXYTAMEX(0,0,1,1,0,0,1,1);
-  loss2online->SetLosXYMCFD(1.011,1.216,1.27,1.88,0,0,1,1);//(0.9781,1.152,1.5,1.5,0,0,1,1);                       
-  loss2online->SetLosXYToT(-0.002373,0.007423,2.27,3.22,0,0,1,1);//(-0.02054,-0.02495,2.5,3.6,0,0,1,1);
-  loss2online->SetEpileup(350.);  // Events with ToT>Epileup are not considered
-  loss2online->SetTrigger(1);     // -1 = no trigger selection
-	loss2online->SetTpat(0);        // if 0, no tpat selection
-	// AoQ calibration :
-	loss2online->SetToFmin(-1000);
-	loss2online->SetToFmin(1000);
-  loss2online->SetTof2InvV_p0(-7.8);
-  loss2online->SetTof2InvV_p1(0.0073);
-	loss2online->SetFlightLength(137);
-	loss2online->SetPos_p0(-11.);
-	loss2online->SetPos_p1(54.7);
-	loss2online->SetDispersionS2(7000);
-	loss2online->SetBrho0_S2toCC(12.);
-	//run->AddTask( loss2online );
+    // S2 Mapped -> Tcal
+    R3BSci2Mapped2Tcal* s2Mapped2Tcal = new R3BSci2Mapped2Tcal("Sci2Map2Tcal", 1);
+    // run->AddTask(s2Mapped2Tcal);
 
-	// --- -------------------------------------------------------- ---	
-  // --- Initialize --------------------------------------------- --- 
-	// --- -------------------------------------------------------- ---	
-  run->Init();
-  rtdb->print();
-  FairLogger::GetLogger()->SetLogScreenLevel("INFO");    
-  //FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
-  //FairLogger::GetLogger()->SetLogScreenLevel("DEBUG");
-  //FairLogger::GetLogger()->SetLogScreenLevel("DEBUG1");
-  //FairLogger::GetLogger()->SetLogScreenLevel("ERROR");
+    // --- -------------------------------------------------------- ---
+    // --- Add online tasks --------------------------------------- ---
+    // --- -------------------------------------------------------- ---
 
-	// --- -------------------------------------------------------- ---	
-  // --- Run ---------------------------------------------------- --- 
-	// --- -------------------------------------------------------- ---	
-  run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
-	//rtdb->saveOutput();
+    // --- LOS STANDALONE
+    R3BOnlineSpectraLosStandalone* r3bOnlineSpectra =
+        new R3BOnlineSpectraLosStandalone("OnlineSpectraLosStandalone", 1);
+    r3bOnlineSpectra->SetNofLosModules(1); // 1 or 2 LOS detectors
+    //  Set parameters for X,Y calibration
+    //  (offsetX, offsetY,VeffX,VeffY)
+    r3bOnlineSpectra->SetLosXYTAMEX(0, 0, 1, 1, 0, 0, 1, 1);
+    r3bOnlineSpectra->SetLosXYMCFD(1.011, 1.216, 1.27, 1.88, 0, 0, 1, 1);       //(0.9781,1.152,1.5,1.5,0,0,1,1);
+    r3bOnlineSpectra->SetLosXYToT(-0.002373, 0.007423, 2.27, 3.22, 0, 0, 1, 1); //(-0.02054,-0.02495,2.5,3.6,0,0,1,1);
+    r3bOnlineSpectra->SetEpileup(350.); // Events with ToT>Epileup are not considered
+    r3bOnlineSpectra->SetTrigger(1);    // -1 = no trigger selection
+    r3bOnlineSpectra->SetTpat(0);       // if 0, no tpat selection
+    run->AddTask(r3bOnlineSpectra);
 
-	// --- -------------------------------------------------------- ---	
-	// --- Finish ------------------------------------------------- ---
-	// --- -------------------------------------------------------- ---	
-  timer.Stop();
-  Double_t rtime = timer.RealTime();
-  Double_t ctime = timer.CpuTime();
-  cout << endl << endl;
-  cout << "Macro finished succesfully." << endl;
-  cout << "Output file is " << outputFilename << endl;
-  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl << endl;
+    // --- SCI2 STANDALONE
+    R3BOnlineSpectraSci2* s2online = new R3BOnlineSpectraSci2();
+    s2online->SetNbDetectors(1);
+    s2online->SetNbChannels(3);
+    run->AddTask(s2online);
+
+    // --- LOS VS SCI2
+    R3BOnlineSpectraLosVsSci2* loss2online = new R3BOnlineSpectraLosVsSci2("OnlineSpectraLosVsSci2", 1);
+    loss2online->SetNofLosModules(1); // 1 or 2 LOS detectors
+    //  Set parameters for X,Y calibration
+    //  (offsetX, offsetY,VeffX,VeffY)
+    loss2online->SetLosXYTAMEX(0, 0, 1, 1, 0, 0, 1, 1);
+    loss2online->SetLosXYMCFD(1.011, 1.216, 1.27, 1.88, 0, 0, 1, 1);       //(0.9781,1.152,1.5,1.5,0,0,1,1);
+    loss2online->SetLosXYToT(-0.002373, 0.007423, 2.27, 3.22, 0, 0, 1, 1); //(-0.02054,-0.02495,2.5,3.6,0,0,1,1);
+    loss2online->SetEpileup(350.);                                         // Events with ToT>Epileup are not considered
+    loss2online->SetTrigger(1);                                            // -1 = no trigger selection
+    loss2online->SetTpat(0);                                               // if 0, no tpat selection
+    // AoQ calibration :
+    loss2online->SetToFmin(-1000);
+    loss2online->SetToFmin(1000);
+    loss2online->SetTof2InvV_p0(-7.8);
+    loss2online->SetTof2InvV_p1(0.0073);
+    loss2online->SetFlightLength(137);
+    loss2online->SetPos_p0(-11.);
+    loss2online->SetPos_p1(54.7);
+    loss2online->SetDispersionS2(7000);
+    loss2online->SetBrho0_S2toCC(12.);
+    // run->AddTask( loss2online );
+
+    // --- -------------------------------------------------------- ---
+    // --- Initialize --------------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    run->Init();
+    rtdb->print();
+    FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+    // FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
+    // FairLogger::GetLogger()->SetLogScreenLevel("DEBUG");
+    // FairLogger::GetLogger()->SetLogScreenLevel("DEBUG1");
+    // FairLogger::GetLogger()->SetLogScreenLevel("ERROR");
+
+    // --- -------------------------------------------------------- ---
+    // --- Run ---------------------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
+    // rtdb->saveOutput();
+
+    // --- -------------------------------------------------------- ---
+    // --- Finish ------------------------------------------------- ---
+    // --- -------------------------------------------------------- ---
+    timer.Stop();
+    Double_t rtime = timer.RealTime();
+    Double_t ctime = timer.CpuTime();
+    cout << endl << endl;
+    cout << "Macro finished succesfully." << endl;
+    cout << "Output file is " << outputFilename << endl;
+    cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl << endl;
 }
-
